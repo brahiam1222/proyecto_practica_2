4bc980943deaba04837d8776dee70152801291dc
brahi <brahi@DESKTOP-8V1T4Q4>
2022-08-31 16:23:03 -0500

    Avance 31082022

--- vistas/plantilla.php
+++ vistas/plantilla.php
@@ -12,6 +12,7 @@
   <!-- <script src="https://kit.fontawesome.com/eeaede5d80.js" crossorigin="anonymous"></script> -->
   <link rel="stylesheet" href="css/bootstrap.min.css">
   <link rel="stylesheet" href="css/style.css">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
   <title>Principal</title>
 </head>
