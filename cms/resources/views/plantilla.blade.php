d3e07f6f60e9698fd1253c5a0b042f7b4aac20c4
brahi <brahi@DESKTOP-8V1T4Q4>
2022-09-15 16:28:43 -0500

    Avance 15092022428 *Posibilidad 2*

--- cms/resources/views/plantilla.blade.php
+++ cms/resources/views/plantilla.blade.php
@@ -5,30 +5,29 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- ========== Start Section  PLUGIN CSS ========== -->
-     <!-- Google Font: Source Sans Pro -->
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
-  <!-- Font Awesome -->
-  <link rel="stylesheet" href="{{url('/')}}js/plugins/fontawesome-free/css/all.min.css">
-  <!-- Ionicons -->
-  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
-  <!-- Tempusdominus Bootstrap 4 -->
-  <link rel="stylesheet" href="{{url('/')}}js/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
-  <!-- iCheck -->
-  <link rel="stylesheet" href="{{url('/')}}js/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
-  <!-- JQVMap -->
-  <link rel="stylesheet" href="{{url('/')}}js/plugins/jqvmap/jqvmap.min.css">
-  <!-- Theme style -->
-  <link rel="stylesheet" href="{{url('/')}}js/dist/css/adminlte.min.css">
-  <!-- overlayScrollbars -->
-  <link rel="stylesheet" href="{{url('/')}}js/plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
-  <!-- Daterange picker -->
-  <link rel="stylesheet" href="{{url('/')}}js/plugins/daterangepicker/daterangepicker.css">
-  <!-- summernote -->
-  <link rel="stylesheet" href="{{url('/')}}js/plugins/summernote/summernote-bs4.min.css">
+    {{-- Fontawesome --}}
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"
+        integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    {{-- Bootstrap 4 --}}
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    {{-- SCROLL CSS --}}
+    <link rel="stylesheet" href="{{ url('/') }}/css/plugins/OverlayScrollbars.min.css">
+
+    {{-- ADMINLTE --}}
+    <link rel="stylesheet" href="{{ url('/') }}/css/plugins/adminlte.min.css">
+
+
+    {{-- Google Fonts --}}
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+
     <!-- ========== End Section PLUGIN CSS ========== -->
 
     
-
 
 
 
@@ -41,49 +40,31 @@
     <div class="wrapper">
         @include('modulos.header')
         @include('modulos.sidebar')
+        @include('paginas.inicio')
         @include('modulos.footer')
-        
+        nuevo
     </div>
-
 
     <!-- ========== Start Section PLUGINS JS ========== -->
 
-    <!-- jQuery -->
-<script src="{{url('/')}}js/plugins/jquery/jquery.min.js"></script>
-<!-- jQuery UI 1.11.4 -->
-<script src="{{url('/')}}js/plugins/jquery-ui/jquery-ui.min.js"></script>
-<!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
-{{-- <script>
-  $.widget.bridge('uibutton', $.ui.button)
-</script> --}}
-<!-- Bootstrap 4 -->
-<script src="{{url('/')}}js/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-<!-- ChartJS -->
-<script src="{{url('/')}}js/plugins/chart.js/Chart.min.js"></script>
-<!-- Sparkline -->
-<script src="{{url('/')}}js/plugins/sparklines/sparkline.js"></script>
-<!-- JQVMap -->
-<script src="{{url('/')}}js/plugins/jqvmap/jquery.vmap.min.js"></script>
-<script src="{{url('/')}}js/plugins/jqvmap/maps/jquery.vmap.usa.js"></script>
-<!-- jQuery Knob Chart -->
-<script src="{{url('/')}}js/plugins/jquery-knob/jquery.knob.min.js"></script>
-<!-- daterangepicker -->
-<script src="{{url('/')}}js/plugins/moment/moment.min.js"></script>
-<script src="{{url('/')}}js/plugins/daterangepicker/daterangepicker.js"></script>
-<!-- Tempusdominus Bootstrap 4 -->
-<script src="{{url('/')}}js/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
-<!-- Summernote -->
-<script src="{{url('/')}}js/plugins/summernote/summernote-bs4.min.js"></script>
-<!-- overlayScrollbars -->
-<script src="{{url('/')}}js/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
-<!-- AdminLTE App -->
-<script src="{{url('/')}}js/dist/js/adminlte.js"></script>
-<!-- AdminLTE for demo purposes -->
-<script src="{{url('/')}}js/dist/js/demo.js"></script>
-<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-<script src="{{url('/')}}js/dist/js/pages/dashboard.js"></script>
+    {{-- JQUERY --}}
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js" type="text/javascript"></script>
+
+    {{-- BOOTSTRAP JS --}}
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+
+    {{-- FONT AWESOME JS --}}
+    <script src="https://kit.fontawesome.com/eeaede5d80.js" crossorigin="anonymous"></script>
+
+    {{-- SCROLL JS --}}
+    <script src="{{ url('/') }}/js/plugins/jquery.overlayScrollbars.min.js"></script>
+
+    {{-- ADMINLTE JS --}}
+    <script src="{{ url('/') }}/js/plugins/adminlte.js"></script>
+
 
     <!-- ========== End Section PLUGINS JS ========== -->
+
 </body>
 
 </html>
